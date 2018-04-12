c15afe60f3d728d60828538a540b91a4d12f5d2a
zyjyuyan <zyjyuyan@163.com>
2018-04-12 11:09:28 +0800

    first html page

--- Clubevent/settings.py
+++ Clubevent/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'main',
+    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
